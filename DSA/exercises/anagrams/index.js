5452377150d2de45a478d7e42cf038c25b537062
Shaqueb <shaqeb1@gmail.com>
2022-02-04 12:16:40 +0530

    anagrams

--- DSA/exercises/anagrams/index.js
+++ DSA/exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// Solution 1
 
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if(Object.keys(aCharMap).length !== Object.keys(bCharMap)) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false
+//     }
+//   }
+//   return true;
+// }
+
+// // Helper Function
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, '').toLowercase()) {
+//     charMap(char) = charMap + 1 || 1;
+//   }
+// }
+
+// Solution 2
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+// Helper Function
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 module.exports = anagrams;
