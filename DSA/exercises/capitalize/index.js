616f4ad9fca21f304dd6e8fe3723ea12ad9c5e73
Shaqueb <shaqeb1@gmail.com>
2022-02-07 21:56:37 +0530

    Drum-Kit

--- DSA/exercises/capitalize/index.js
+++ DSA/exercises/capitalize/index.js
@@ -6,7 +6,15 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+// Solution 1
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
 
 module.exports = capitalize;
