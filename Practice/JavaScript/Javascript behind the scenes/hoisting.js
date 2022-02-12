cd3a8938e8fe6e12ed160d20bab460f4f6b4e1e2
Shaqueb <shaqeb1@gmail.com>
2022-02-13 01:08:02 +0530

    Javascript behind the scenes

---
+++ Practice/JavaScript/Javascript behind the scenes/hoisting.js
@@ -0,0 +1,5 @@
+/*
+Hosting: Makes some types of variables accessible/usable in the code before they are actually declared. "Variables lifted to the top of their scope"
+
+Before execution, code is scanned for variable declarations, and for each variable a new property is created in the variable environment object.
+*/
---
+++ Practice/JavaScript/Javascript behind the scenes/scope.js
@@ -0,0 +1,11 @@
+/*
+Scope Concepts
+Scoping: How our program's variables are organized and accessed. "Where do variables live?" "Where can we access certain variables and where not?"
+
+Lexical scoping: Scoping is controlled by placements of functions and blocks in the code.
+
+Scope: Space or environment in which certain variables is declared (variable environment in case of functions). There is global scope, function scope and block scope.
+
+Scope of a variable: Region of our code where certain variable can be accessed.
+
+*/
