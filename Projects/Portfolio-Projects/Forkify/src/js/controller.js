c26884e8e50a2b5f65bec2907ab1c2afb3c9d9d1
Shaqueb <shaqeb1@gmail.com>
2022-03-03 21:29:18 +0530

    Django

--- Projects/Portfolio-Projects/Forkify/src/js/controller.js
+++ Projects/Portfolio-Projects/Forkify/src/js/controller.js
@@ -10,4 +10,30 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+// API KEY - 7457ba1e-9e2c-46c7-af7c-292206f4c8a5
+
+// https://forkify-api.herokuapp.com/api/v2/recipes/:id
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch('');
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
