b4667b0ae104e2d4fe217f5760694b77a41b1416
Shaqueb <shaqeb1@gmail.com>
2022-03-21 20:18:56 +0530

    error-handling setup

--- Projects/Portfolio-Projects/YelpCamp/models/campground.js
+++ Projects/Portfolio-Projects/YelpCamp/models/campground.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 // Campground Schema Setup
 const CampgroundSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
 });
--- Projects/Portfolio-Projects/YelpCamp/seeds/index.js
+++ Projects/Portfolio-Projects/YelpCamp/seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const axios = require('axios');
 
 // Mongoose Connection
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -19,15 +20,38 @@ db.once('open', () => {
 // Sample data from seedHelper.js
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function seedImg() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: 'QOaYBg6OfeF73hoMQNnA40HlchuU54gXRctJi7N4a3U',
+        collections: 1114848,
+      },
+    });
+    return resp.data.urls.small;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 // Random Data Generator for Campgrounds
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
+  for (let i = 0; i < 20; i++) {
+    // setup
+    const placeSeed = Math.floor(Math.random() * places.length);
+    const descriptorsSeed = Math.floor(Math.random() * descriptors.length);
+    const citySeed = Math.floor(Math.random() * cities.length);
+
+    // seed data into campground
     const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`,
+      image: await seedImg(),
+      title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+      location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+      description:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
     });
+
     await camp.save();
   }
 };
