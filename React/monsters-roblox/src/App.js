f9f4437a4cc8011cf039a8e806b7e8bd81f8519d
Shaqueb <shaqeb1@gmail.com>
2022-04-02 21:02:15 +0530

    Monster Rolodex

--- React/monsters-roblox/src/App.js
+++ React/monsters-roblox/src/App.js
@@ -1,33 +1,52 @@
 import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: 1,
-        },
-        {
-          name: 'Frank',
-          id: 2,
-        },
-        {
-          name: 'Jacky',
-          id: 3,
-        },
-      ],
+      monsters: [],
+      searchField: '',
     };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-        })}
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search-monsters'
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
