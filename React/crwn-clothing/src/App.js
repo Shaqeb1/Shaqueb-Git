18a145f424420592d59dd520338c950aba7c163d
Shaqueb <shaqeb1@gmail.com>
2022-04-23 14:53:44 +0530

    firebase

--- React/crwn-clothing/src/App.js
+++ React/crwn-clothing/src/App.js
@@ -1,7 +1,17 @@
-import Directory from './components/directory/directory.component'
+import Home from './routes/home/home.component'
+import { Routes, Route } from 'react-router-dom'
+import Navigation from './routes/navigation/navigation.component'
+import SignIn from './routes/sign-in/sign-in.component'
 
 const App = () => {
-  return <Directory />
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
---
+++ React/crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import './navigation.styles.scss'
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='links-container'>
+          <Link className='nav-link' to='/shop'>
+            Shop
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
