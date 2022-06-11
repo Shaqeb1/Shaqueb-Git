c9685c408bc50f873de2f8b066ea0dcdf56dca89
Shaqeb <shaqeb1@gmail.com>
2022-06-11 15:00:33 +0530

    folder-changes

---
+++ CSS/Tailwind/Tailwind-Projects/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768',
+      lg: '976',
+      xl: '1440,'
+    },
+    extend: {
+      colors: {
+        darkBlue: '#2F8F9D',
+        lessDarkBlue: '#3BACB6',
+        lightBlue: '#82DBD8',
+        veryLightBlue: '#B3E8E5'
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
