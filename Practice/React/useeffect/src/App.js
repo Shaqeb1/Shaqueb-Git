2eeb2e5510397c2802366d50ec88cb75b7fb8849
Shaqeb <shaqeb1@gmail.com>
2022-06-13 14:08:21 +0530

    transitions

--- Practice/React/useeffect/src/App.js
+++ Practice/React/useeffect/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
    <div className='container'>
      <div className='row justify-content-center align-items-center text-center shadow-sm p-3 mb-5 bg-body rounded'><h1 className='heading-primary'>{value}</h1>
-     <button className='btn btn-primary sm' onClick={() => setValue(value + 1)}>Click Me</button>
+     <button className='btn btn-primary-sm' onClick={() => setValue(value + 1)}>Click Me</button>
      </div>
    </div>
   )
