91aacab4517bb04596a5fe196cd608ba335658c6
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-07-18 20:56:55 +0530

    firebase setup threads

--- Projects/Portfolio-Projects/threads/src/utils/firebase/firebase.utils.js
+++ Projects/Portfolio-Projects/threads/src/utils/firebase/firebase.utils.js
@@ -16,3 +16,16 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
 
+const provider = new GoogleAuthProvider()
+
+provider.setCustomParameters({
+  prompt: 'select_account'
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth , provider)
+
+export const db = getFirestore()
+
+
+
