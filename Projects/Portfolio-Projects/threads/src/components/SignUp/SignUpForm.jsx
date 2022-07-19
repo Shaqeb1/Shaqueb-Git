f882fcafdeb82cd4d42758df5c3b733bcb02e23e
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-07-19 13:16:35 +0530

    firebase auth

--- Projects/Portfolio-Projects/threads/src/components/SignUp/SignUpForm.jsx
+++ Projects/Portfolio-Projects/threads/src/components/SignUp/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import {  createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 import FormInput from '../FormInput/FormInput'
 import './SignUpForm.css'
 const defaultFormFields = {
@@ -22,29 +22,13 @@ const SignUpForm = () => {
         setFormFields(defaultFormFields)
     }
 
-    const handleSubmit = async(event) => {
-        event.preventDefault()
-        if(password !== confirmPassword) {
-            alert('Password do not match')
-            return
-        }
 
-        try {
-            const {user} = await createAuthUserWithEmailAndPassword(email, password)
-            await createUserDocumentFromAuth(user, {displayName})
-            resetFormFields()
-        } catch (error) {
-            if(error.code === 'auth/email already in use') {
-                alert('Cannot create user email already in use')
-            }
-        }
-    }
 
   return (
     <div className='signUp-container'>
         <h2 className='signUp-heading'>Don't have an account?</h2>
         <span className='signUp-span'>Sign up with your email and password</span>
-        <form onSubmit={handleSubmit}>
+        <form>
             <FormInput label="Display Name" type='text' required onChange={handleChange} name='displayName' value={displayName}/>
             <FormInput label="Email" type='email' required onChange={handleChange} name='email' value={email}/>
             <FormInput label="Password" type='password' required onChange={handleChange} name='password' value={password}/>
--- Projects/Portfolio-Projects/threads/src/utils/firebase/firebase.utils.js
+++ Projects/Portfolio-Projects/threads/src/utils/firebase/firebase.utils.js
@@ -27,5 +27,27 @@ export const signInWithGooglePopup = () => signInWithPopup(auth , provider)
 
 export const db = getFirestore()
 
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  const userSnapshot = await getDoc(userDocRef)
+  console.log(userSnapshot)
+
+  if(!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName, email , createdAt
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userDocRef
+
+}
 
 
