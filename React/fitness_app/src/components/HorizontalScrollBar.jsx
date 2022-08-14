c52d5d546619ace6c34a65a0d381d696922e7305
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-08-14 19:15:37 +0530

    react

---
+++ React/fitness_app/src/components/HorizontalScrollBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box} from '@mui/material'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+import BodyPart from './BodyPart'
+
+const HorizontalScrollBar = ({data, bodyPart, setBodyPart}) => {
+  return (
+    <ScrollMenu>
+        {data.map((item) => <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m='0 40px'>
+            <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+        </Box>)}
+    </ScrollMenu>
+  )
+}
+
+export default HorizontalScrollBar
