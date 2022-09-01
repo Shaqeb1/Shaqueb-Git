2adcb58ddf2da388b2bf12dd8b2e7182f7b7be7f
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-09-01 15:03:38 +0530

    back to the grind

--- React/fitness_app/src/components/BodyPart.jsx
+++ React/fitness_app/src/components/BodyPart.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import {Stack, Typography} from '@mui/material'
-import icon from '../assets/icons/gym.png'
+import React from 'react';
+import { Stack, Typography } from '@mui/material';
+import Icon from '../assets/icons/gym.png';
 
-const BodyPart = ({item, setBodyPart, bodyPart}) => {
-  return (
-   <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card' 
-   sx={{borderTop: bodyPart === item ? '4px solid #ff2625' : '', 
-   backgroundColor: '#fff', 
-   borderBottomLeftRadius: '20px', 
-   width: '270px',
-   height: '280px',
-   cursor: 'pointer', 
-   gap: '47px'
-   }}>
-        <img src={icon} alt="dumbbell" style={{width: '40px', height: '40px'}} />
-    </Stack>
-  )
-}
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+    sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    onClick={() => {
+      setBodyPart(item);
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+    }}
+  >
+    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
+  </Stack>
+);
 
-export default BodyPart
+export default BodyPart;
