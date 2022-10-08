21e66e2046d3edbd82626ff31e6d2606d678aa90
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-10-08 14:22:12 +0530

    cleanup function

--- React/Practice/react-projects-master/01-birthday-reminder/setup/src/List.js
+++ React/Practice/react-projects-master/01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const {id, name, age, image} = person
+        return (
+          <article
+            className='person'
+            key={id}
+          >
+            <img
+              src={image}
+              alt={name}
+            />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
     </>
   );
 };
