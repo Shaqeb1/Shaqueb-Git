0880d6076a4426e2eb27a360c45f39d88c3cab51
Shaqeb <88718301+Shaqeb1@users.noreply.github.com>
2022-11-07 19:18:28 +0530

    react-advance projects

--- react-projects-master/16-markdown-preview/setup/src/App.js
+++ react-projects-master/16-markdown-preview/setup/src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react"
+import ReactMarkdown from "react-markdown"
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState("## markdown preview")
+  return (
+    <main>
+      <section className='markdown'>
+        <textarea
+          value={markdown}
+          onChange={e => {
+            setMarkdown(e.target.value)
+          }}
+          className='input'
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
 }
 
 export default App
