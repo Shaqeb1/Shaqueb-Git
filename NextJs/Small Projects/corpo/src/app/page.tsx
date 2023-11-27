ba6f7c59a9b7ecdd3e4ba14a550cc2e90c6189a0
Shaqueb <88718301+Shaqeb1@users.noreply.github.com>
2023-11-27 22:18:48 +0530

    prisma setup

--- NextJs/Small Projects/corpo/src/app/page.tsx
+++ NextJs/Small Projects/corpo/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Hero from '@/components/hero'
+import homeImg from 'public/images/home.jpg'
 
 export default function Home() {
   return (
-   <div>Home Page</div>
+   <Hero imgData={homeImg}
+    imgAlt='car factory'
+    title='Professional Cloud Hosting'
+   />
   )
 }
